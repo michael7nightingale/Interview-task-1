1faca3604eea434d6ba4bfd77209979615e6198e
michael7nightingale <122199262+michael7nightingale@users.noreply.github.com>
2023-04-11 10:40:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ CELEBRATION_GENERATOR = {
     "MESSAGE": """Напиши поздравление для друга __name__ в его день рождения, который родился 
     __birthday__. Сделай это душевно и лаконично, всё таки это день рождения.""",
     "TOKEN": '123',
+    "NAME_COLUMN": "name",
+    "BIRTHDAY_COLUMN": "birthday",
+
 }
 
 
